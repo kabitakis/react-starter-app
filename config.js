3cc2be59ec4616afc553133cd2aba51dba36b48b
Nikos Kampitakis <nikosk@imgzine.com>
2015-12-05 19:55:30 +0200

    Improved config file

--- config.js
+++ config.js
@@ -1,28 +1,33 @@
+var _ = require('lodash');
+
 var config = {
+  default: {
+    language: 'en',
+    params: {
+      msg: 'Ahoy!'
+    }
+  },
   local: {
     mode: 'local',
-    port: 3001,
-    language: 'en',
+    port: 3003,
     memcached_host: "127.0.0.1:11211",
     deployType:'development'
   },
   staging: {
     mode: 'staging',
-    port: 3001,
-    language: 'en',
+    port: 3003,
     memcached_host: "127.0.0.1:11211",
     deployType:'staging'
   },
   production: {
     mode: 'production',
-    port: 3001,
-    language: 'en',
+    port: 3003,
     memcached_host: "127.0.0.1:11211",
     deployType:'production'
   }
 }
 
 module.exports = function(mode) {
-    console.log("CONFIG MODE:", process.env.NODE_ENV);
-    return config[mode || process.env.NODE_ENV || 'local'] || config.local;
+  console.log("CONFIG MODE:", mode || process.env.NODE_ENV || 'local');
+  return _.extend(config[mode || process.env.NODE_ENV || 'local'], config.default);
 }();
